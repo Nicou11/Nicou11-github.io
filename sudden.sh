6ed4fa56a4f0787b97985cf8eb17245f7dcba4be
Your Name <you@example.com>
2024-07-11 10:10:36 +0900

    date

--- sudden.sh
+++ sudden.sh
@@ -5,7 +5,11 @@ FILE=~/tmp/_SUDDEN
 while true
 do
 	if [ -e "$FILE" ]; then
-	    echo "check $FILE"
+	    #echo "check $FILE"
+	    echo "SUDDEN!!!"
+        else
+	    clear
+            date	    
 	fi    
 	#sleep 3
 done
